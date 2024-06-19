d0f0b5e7ae44227e4a9fee0deb07b8c7eeceba3a
K-Dawg92 <kevin.osei.yaw@gmail.com>
2024-06-19 11:38:35 +0200

    Ajoutez des fichiers projet.

---
+++ Character Game/Character.cs
@@ -0,0 +1,42 @@
+﻿using Character_Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Game
+{
+    public class Character
+    {
+        private string _name;
+        private int _pointDeVie;
+        private int _forceDattaq;
+        private int _forceDefence;
+
+        // constructeur
+
+        public Character(string name, int pointDeVie, int forceDattaq, int forceDefence)
+        {
+            _name = name;
+            _forceDattaq = forceDattaq;
+            _forceDefence = forceDefence;
+            _pointDeVie = pointDeVie;
+        }  
+        public bool isAlive()
+        {
+            return _pointDeVie > 0;
+        }
+        public void Attack(Character target)
+        {
+            target._pointDeVie =( target._pointDeVie) - (_forceDattaq - target._forceDefence);
+        }
+
+        public string GetName() 
+        { 
+            return _name; 
+        
+       }
+    }
+}
+
---
+++ Character Game/Program.cs
@@ -0,0 +1,23 @@
+﻿using Character_Game;
+using System;
+Character personnage1 = new Character("Jon",100,200,150);
+Character personnage2 = new Character("Louisa", 100,200,135);
+
+
+while(personnage1.isAlive() && personnage2.isAlive())
+{
+    personnage1.Attack(personnage2);
+    personnage2.Attack(personnage1);
+
+}
+
+if (personnage1.isAlive())
+{
+    Console.WriteLine($"The winner is {personnage1.GetName()}");
+}
+
+else
+{
+    Console.WriteLine($"The winner is {personnage2.GetName()}");
+
+}
